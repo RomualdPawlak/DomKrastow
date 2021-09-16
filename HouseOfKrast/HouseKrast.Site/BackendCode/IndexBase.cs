43ee4081f5bb25997eab7bdb3513add43d20fbd1
Klimek, Mariusz <klimcio@gmail.com>
2021-09-16 19:33:35 +0200

    First prototype

---
+++ HouseOfKrast/HouseKrast.Site/BackendCode/IndexBase.cs
@@ -0,0 +1,35 @@
+﻿using HouseKrast.Site.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HouseKrast.Site.BackendCode
+{
+    public class IndexBase : ComponentBase
+    {
+        public int StartYear = 2100;
+        public int EndYear = 3000;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        public TimelineItem[] KrastTimeline;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //KrastTimeline = new TimelineItem[] { };
+            //var options = new JsonSerializerOptions()
+            //{
+            //    ReferenceHandler = ReferenceHandler.Preserve,
+            //    PropertyNameCaseInsensitive = true
+            //};
+            //KrastTimeline = await Http.GetFromJsonAsync<TimelineItem[]>("data/timeline.json");
+            //var file = await Http.GetStringAsync("data/timeline.json");
+            //Console.WriteLine(file);
+        }
+    }
+}
---
+++ HouseOfKrast/HouseKrast.Site/Models/TimelineItem.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseKrast.Site.Models
+{
+    public class TimelineItem
+    {
+        public int Year { get; set; }
+        public string Title { get; set; }
+    }
+}
