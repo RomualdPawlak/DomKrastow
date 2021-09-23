430ad8e469b64362a1bbc95239dfa162c06955b7
Mariusz Klimek <klimcio@gmail.com>
2021-09-23 23:02:44 +0200

    Books sidebar

--- HouseOfKrast/HouseKrast.Webpage/Models/Book.cs
+++ HouseOfKrast/HouseKrast.Webpage/Models/Book.cs
@@ -1,10 +1,13 @@
-﻿namespace HouseKrast.Webpage.Models
+﻿using System;
+
+namespace HouseKrast.Webpage.Models
 {
     public class Book
     {
         public string Title { get; set; }
-        public string PublishDate { get; set; }
+        public DateTime PublishDate { get; set; }
         public string CoverMini { get; set; }
         public string Cover { get; set; }
+        public string PublisherLink { get; set; }
     }
 }
--- HouseOfKrast/HouseKrast.Webpage/Pages/BooksBase.cs
+++ HouseOfKrast/HouseKrast.Webpage/Pages/BooksBase.cs
@@ -1,5 +1,8 @@
 ﻿using HouseKrast.Webpage.Models;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -11,11 +14,23 @@ namespace HouseKrast.Webpage.Pages
         [Inject]
         public HttpClient Http { get; set; }
 
-        protected Book[] Books { get; set; }
+        protected IList<Book> PublishedBooks { get; set; }
+
+        protected IList<Book> NotPublishedBooks { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Books = await Http.GetFromJsonAsync<Book[]>("data/books.json");
+            var _books = await Http.GetFromJsonAsync<Book[]>("data/books.json");
+
+            PublishedBooks = _books
+                .Where(x => x.PublishDate <= DateTime.Now)
+                .OrderByDescending(x => x.PublishDate)
+                .ToList();
+
+            NotPublishedBooks = _books
+                .Where(x => x.PublishDate > DateTime.Now)
+                .OrderByDescending(x => x.PublishDate)
+                .ToList();
         }
     }
 }
