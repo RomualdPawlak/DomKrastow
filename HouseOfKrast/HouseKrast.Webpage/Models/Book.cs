47246d2185be569e230965765a0bca17d16f9cd4
Mariusz Klimek <klimcio@gmail.com>
2021-09-23 21:42:52 +0200

    Sidebar with books, part 1

---
+++ HouseOfKrast/HouseKrast.Webpage/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseKrast.Webpage.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string PublishDate { get; set; }
+        public string CoverMini { get; set; }
+        public string Cover { get; set; }
+    }
+}
---
+++ HouseOfKrast/HouseKrast.Webpage/Pages/BooksBase.cs
@@ -0,0 +1,21 @@
+﻿using HouseKrast.Webpage.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace HouseKrast.Webpage.Pages
+{
+    public class BooksBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        protected Book[] Books { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = await Http.GetFromJsonAsync<Book[]>("data/books.json");
+        }
+    }
+}
